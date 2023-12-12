d7e863ad9c86eb8b5c6328de36fb72336d9a29d1
nhuamani <nhuamani.c7@gmail.com>
2023-12-12 13:39:17 -0500

    adding multiplication table print

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello gfhg");
+let autputMessage = ''
+const table = 5
+const headerMessage = `
+===================================
+            Tabla del ${table}      
+===================================
+`
+console.log(headerMessage)
+for (let i = 0; i <= 10; i++) {
+    console.log(`${table} x ${i} = ${table * i}`)
+}
